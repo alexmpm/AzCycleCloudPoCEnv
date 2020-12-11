f2ff6eb719b86ac61dc62edeaf27a7aa9accfabf
alexmpm <71778727+alexmpm@users.noreply.github.com>
2020-12-11 16:38:17 +0100

    Create deploy.ps1

---
+++ scripts/deploy.ps1
@@ -0,0 +1,15 @@
+param (
+    # Parameter help description
+    [Parameter(Mandatory=$true)]
+    [string]$rgName
+)#end param
+
+$rgExists = az group exists -n $rgName
+
+if($rgExists -eq $false)
+{
+    Write-Output "starting deployment..."
+}
+else {
+    Write-Output "Resource Group with this name already exists under this subscription"
+}
